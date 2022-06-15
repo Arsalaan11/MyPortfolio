0f7de757c48c6e84f7414abe6c27a13dad5118fc
Arsalaan Ansari <49878283+Arsalaan11@users.noreply.github.com>
2022-06-15 14:47:51 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,6 +18,26 @@ for (var i=0; themeDots.length > i; i++){
 		setTheme(mode)
 	})
 }
+$(document).ready(function(){
+
+$('#itemslider').carousel({ interval: 3000 });
+
+$('.carousel-showmanymoveone .item').each(function(){
+var itemToClone = $(this);
+
+for (var i=1;i<6;i++) {
+itemToClone = itemToClone.next();
+
+if (!itemToClone.length) {
+itemToClone = $(this).siblings(':first');
+}
+
+itemToClone.children(':first-child').clone()
+.addClass("cloneditem-"+(i))
+.appendTo($(this));
+}
+});
+});
 
 function setTheme(mode){
 	if(mode == 'light'){
