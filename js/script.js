aa05936e87dfee3b02b8f7409ff09d9c0d85ce6f
Arsalaan Ansari <49878283+Arsalaan11@users.noreply.github.com>
2024-11-28 12:24:14 +0530

    Update script.js
    changed the path to all color

--- js/script.js
+++ js/script.js
@@ -41,11 +41,11 @@ itemToClone.children(':first-child').clone()
 
 function setTheme(mode){
 	if(mode == 'light'){
-		document.getElementById('theme-style').href = '/css/default.css'
+		document.getElementById('theme-style').href = 'css/default.css'
 	}
 
 	if(mode == 'blue'){
-		document.getElementById('theme-style').href = '/css/blue.css'
+		document.getElementById('theme-style').href = 'css/blue.css'
 	}
 
 	if(mode == 'green'){
